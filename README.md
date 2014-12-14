1004417ca73eba49f6ce67bb8026d7350b437c56
Raheman Vaiya <rvaiya@qplx.org>
2014-12-14 14:51:17 -0500

    Version 1.0.6
     -Added drain
     -Fixed broken passiveStreamGenerator

--- README.md
+++ README.md
@@ -4,13 +4,21 @@
 
 #Usage
 
-	var vutil = require('vinyl-utils');
+```javascript
+  var vutil = require('vinyl-utils');
+  var sass = require('gulp-sass');
+  var minify = require('gulp-minify');
 
-	vutil.collect(gulp.src('testing').pipe(inject))
-		.then(function(files) { /* Do something */ });;
+  vutil.collect(gulp.src('testing').pipe(inject))
+    .then(function(files) { /* Do something */ });;
 
-	gulp.src
-		.pipe(vinyl.passiveStreamGenerator(function() {
-			/* Do something */
-		})());
+  gulp.src('input/*')
+    .pipe(vutil.sideeffect(function(file) {
+      console.log(file.path);
+    })())
+	.pipe(vutil.drain());
 
+  gulp.src('sass/*')
+    .pipe(vutil.createPipeline(sass, minify))
+    .pipe(gulp.dest('output'));
+```
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var through = require('through2');
 // Custom Streams
 // =================================================================================
 
+
 // Streams with side effects (don't hurt me please)
 
 function passiveStreamGenerator(func) {
@@ -13,7 +14,7 @@ function passiveStreamGenerator(func) {
       this.push(file);
       cb();
       if(func)
-        func();
+        func(file);
     });
   };
 }
@@ -34,8 +35,29 @@ function collect(stream) {
   return p.promise;
 }
 
+//Consumes an array of stream generators and returns a single stream
+
+function createPipeline(generators) {
+    if(!generators || !generators.length) {
+      return passiveStreamGenerator()();
+    }
+    var pipeline = generators.map(function(f) { return f(); });
+    pipeline.reduce(function(a,b) { return a.pipe(b); });
+    return pipeline[0];
+};
+
+// 'Black hole' which consumes all of its input and produces no output (useful for
+// draining the last passiveStream in a chain)
+
+function drain() {
+  return through.obj(function(f,e,d) { d(); });
+}
 
 module.exports = {
   passiveStreamGenerator: passiveStreamGenerator,
+  sideeffect: passiveStreamGenerator,
+  passive: passiveStreamGenerator,
+  createPipeline: createPipeline,
   collect: collect,
+  drain: drain
 };
